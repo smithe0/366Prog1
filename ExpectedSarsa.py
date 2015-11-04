ebfd890a2b74db7c565ccb502d7ad0fb74c32b1d
petersterling1 <psterlin>
2015-11-03 18:58:32 -0700

    Cleaned code up a bit

--- ExpectedSarsa.py
+++ ExpectedSarsa.py
@@ -2,10 +2,10 @@ import blackjack
 from pylab import *
 from numpy import *
 
-numEpisodes = 1000000
+numEpisodes = 500000
 
 states = 0.00001*rand(181,2)
-epsilonbehavior = 0.01
+epsilonbehavior = 1
 epsilontarget = 0.01
 alpha = 0.001
 discount = 1
@@ -15,37 +15,48 @@ discount = 1
 
 returnSum = 0.0
 for episodeNum in range(numEpisodes):
-	G = 0
-	currentstate = blackjack.init()
-	while(currentstate != -1):
-	       
-            rnumber = random.rand()
-	   
-            if rnumber < epsilonbehavior:
-                action = random.randint(2)
+    G = 0
+    #Start a new game of blackjack
+    currentstate = blackjack.init()
+    
+    #Continue this game until the terminal state is reached
+    while(currentstate != -1):
+        #Get a random number between 0 and 1, if its less than epsilon behavior, then explore
+        rnumber = random.rand()
+        if rnumber < epsilonbehavior:
+            action = random.randint(2)
+        else:
+	    #If not exploring, pick the highest action at state S
+            if states[currentstate][0] >= states[currentstate][1]:
+                #hit
+                action = 1
             else:
-	        #Pick highest action at state S
-                if states[currentstate][0] >= states[currentstate][1]:
-                    #hit
-	           action = 1
-                else:
-                    action = 0
-	           #stay 	       
-	       
-            next = blackjack.sample(currentstate, action)
-            G = G + next[0]
-            greedychance = 1-epsilonbehavior
-            highest = argmax(states[next[1]])
+                #stay
+                action = 0	       
+	
+	#Get the next state, get reward and next state
+        next = blackjack.sample(currentstate, action)
+        reward = next[0]
+        nextstate = next[1]
+        #Add to return
+        G = G + reward
+        
+        #Get chance of being greedy
+        greedychance = 1-epsilonbehavior
+        
+        #Get best value at the next state
+        highest = argmax(states[nextstate])
+        
+        #Expected sarsa calculation
+        target = (greedychance * states[nextstate][highest]) + (epsilonbehavior * (0.5*states[nextstate][0] + 0.5*states[nextstate][1]))
             
-            target = (greedychance * states[next[1]][highest]) + (epsilonbehavior * (0.5*states[next[1]][0] + 0.5*states[next[1]][1]))
             
+        states[currentstate][action] = states[currentstate][action] + alpha * (reward + target - states[currentstate][action]) 
             
-            states[currentstate][action] = states[currentstate][action] + alpha * (next[0] + target - states[currentstate][action]) 
-            
-            currentstate = next[1]
+        currentstate = nextstate
             
 	#print "Episode: ", episodeNum, "Return: ", G
 	returnSum = returnSum + G
-	if(episodeNum%10000 == 0):
-	    print "Average return ",episodeNum,": ", returnSum/numEpisodes
+	#if(episodeNum%10000 == 0):
+	    #print "Average return ",episodeNum,": ", returnSum/numEpisodes
 print "Average return: ", returnSum/numEpisodes
