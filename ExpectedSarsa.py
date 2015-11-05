359b7e35fa27569f0c06c34977c7335aec2d6068
Eric Smith <egsmith@ualberta.ca>
2015-11-04 23:38:31 -0700

    Added a change from learning episodes to evauluation episodes. Does not yet print policy after leaning.

--- ExpectedSarsa.py
+++ ExpectedSarsa.py
@@ -2,13 +2,15 @@ import blackjack
 from pylab import *
 import numpy as n
 
-numEpisodes = 1000000
+numEpisodes = 2000000
+learningEpisodes = 1000000
 
 states = 0.00001*n.random.rand(181,2)
 epsilonMu = 0.01
-epsilonPi = 0.01
+epsilonPi = 0.0
 alpha = 0.001
 discount = 1
+epsilon = epsilonMu
 
 #print states
 
@@ -21,12 +23,15 @@ for episodeNum in range(numEpisodes):
     #Continue this game until the terminal state is reached
     while(currentstate != -1):
         #Get a random number between 0 and 1, if its less than epsilon behavior, then explore
+
+        if episodeNum >= learningEpisodes: 
+            epsilon = epsilonPi
+            alpha = 0
+        
         rnumber = n.random.rand()
-        if rnumber < epsilonMu:
-            currentEpsilon = epsilonMu
+        if rnumber < epsilon:
             action = n.random.randint(2)
         else:
-            currentEpsilon = epsilonPi
 	    #If not exploring, pick the highest action at state S
             action = argmax(states[currentstate])       
 	
@@ -38,15 +43,13 @@ for episodeNum in range(numEpisodes):
         G = G + reward
         
         #Get chance of being greedy
-        greedychance = 1-currentEpsilon
+        greedychance = 1-epsilon
         
         #Get best value at the next state
         highest = argmax(states[nextstate])
         
         #Expected sarsa calculation (greedy * best_next_state_action) + (explore * (0.5*next_state_action1 + 0.5*next_state_action2))
-        target = (greedychance * states[nextstate][highest]) + (currentEpsilon * (0.5*states[nextstate][0] + 0.5*states[nextstate][1]))
-            
-            
+        target = (greedychance * states[nextstate][highest]) + (epsilon * (0.5*states[nextstate][0] + 0.5*states[nextstate][1]))
         states[currentstate][action] = states[currentstate][action] + alpha * (reward + target - states[currentstate][action]) 
             
         currentstate = nextstate
